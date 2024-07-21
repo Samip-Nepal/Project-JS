a48919def985ce462d6c7267264606b1914fcf0f
Samip Nepal <samipnepal5@gmail.com>
2024-07-21 16:52:21 +1000

    Draw condition

--- First.js
+++ First.js
@@ -1,9 +1,10 @@
-let game2 = document.querySelectorAll(".game-2"); // Access play buttons
-let game3 = document.querySelector("#reset"); // Access the reset button
+let game2 = document.querySelectorAll(".game-2"); 
+let game3 = document.querySelector("#reset"); 
 let newwinner = document.querySelector(".new-msg");
 let msg = document.querySelector("#msg");
 let newgame = document.querySelector("#new-game");
-let turn0 = true; // One player turn
+let turn0 = true; 
+let count = 0; 
 
 const winrate = [
     [0, 1, 2],
@@ -18,6 +19,7 @@ const winrate = [
 
 Array.from(game2).forEach((game, index) => {
     game.addEventListener("click", () => {
+        count++; 
         if (turn0) {
             game.innerText = "O";
             game.style.fontWeight = "700";
@@ -37,20 +39,34 @@ Array.from(game2).forEach((game, index) => {
 });
 
 const checkwinner = () => {
+    let winnerFound = false; 
+
     for (let pattern of winrate) {
         let pos1 = game2[pattern[0]].innerText;
         let pos2 = game2[pattern[1]].innerText;
         let pos3 = game2[pattern[2]].innerText;
         if (pos1 !== "" && pos2 !== "" && pos3 !== "") {
             if (pos1 == pos2 && pos2 == pos3) {
+                winnerFound = true;
                 winnerfun(pos1);
+                break;
             }
         }
+    }
+
+    if (!winnerFound && count === 9) { 
+        drawfun();
     }
 };
 
 const winnerfun = (winner) => {
     msg.innerText = `Here is the winner "${winner}"`;
+    newwinner.classList.remove("hide");
+    nousebox();
+};
+
+const drawfun = () => {
+    msg.innerText = `It is a draw! chick on new game to restart the game.`;
     newwinner.classList.remove("hide");
     nousebox();
 };
@@ -73,10 +89,11 @@ const yesusebox = () => {
 
 const resetgame = () => {
     turn0 = true;
+    count = 0; 
     yesusebox();
     newwinner.classList.add("hide");
     msg.innerText = "";
 };
 
-game3.addEventListener("click", resetgame); // Corrected to use game3 directly
+game3.addEventListener("click", resetgame); 
 newgame.addEventListener("click", resetgame);
